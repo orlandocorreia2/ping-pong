a57655240d08d6a97689bb235d5783ae883fceff
Orlando Nascimento <orlando.nascimento@capitanigroup.com.br>
2025-04-22 18:38:25 -0300

    test: playwright

--- src/index.js
+++ src/index.js
@@ -1,9 +1,25 @@
 const express = require("express");
+const playwright = require("playwright");
 const app = express();
 const port = 80;
 
-app.get("/ping", (_, res) => {
-  res.json({ message: "pong" });
+app.get("/ping", async (_, res) => {
+  const browser = await playwright.chromium.launch({
+    headless: true,
+  });
+  const context = await browser.newContext(
+    playwright.devices["Desktop Chrome"]
+  );
+  const page = await context.newPage();
+
+  await page.goto(
+    "https://venda-imoveis.caixa.gov.br/sistema/detalhe-imovel.asp?hdnOrigem=index&hdnimovel=10176203"
+  );
+  const photo_link_src = await page.locator("#preview").getAttribute("src");
+  await context.close();
+  await browser.close();
+
+  res.json({ message: "pong", photo_link_src });
 });
 
 app.listen(port, () => {
