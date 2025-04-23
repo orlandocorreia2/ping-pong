fef5979cc89e66473d6557a0d02cc3d513fd6064
Orlando Nascimento <orlando.nascimento@capitanigroup.com.br>
2025-04-22 23:39:20 -0300

    feat: docker-compose

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ const port = 3000;
 
 app.get("/ping", async (_, res) => {
   const browser = await playwright.chromium.launch({
-    headless: true,
+    headless: false,
   });
   const context = await browser.newContext(
     playwright.devices["Desktop Chrome"]
